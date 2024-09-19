c7f4ff27a11108ed7a54a53be0adb1e3be8f5c09
Marco Chan <marco.chan@newtrek.com.hk>
2024-09-19 15:16:12 +0800

    sidebar and rootlayout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
-import Header from '../components/Header/Header'
+import Header from "../components/Header/Header";
+import Sidebar from "../components/Sidebar/Sidebar";
+
+import styles from "./RootLayout.module.css";
 
 function RootLayout() {
-  return (
-    <div>
-      <Header/>
-      <Outlet/>
-    </div>
-  )
+    return (
+        <div>
+            <Header />
+            <main>
+                <div className={styles.mainContainer}>
+                    <div className={styles.outlet}>
+                        <Outlet />
+                    </div>
+                    <div className={styles.sidebar}>
+                        <Sidebar />
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
 }
 
-export default RootLayout
+export default RootLayout;
