39479a4963f7b095271565b920caf201eda2dd24
Marco Chan <marco.chan@newtrek.com.hk>
2024-09-20 15:22:54 +0800

    header and layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,23 +5,24 @@ import logo from "../../images/logo.svg";
 function Header() {
     return (
         <div className={styles.header}>
-            <div className={styles.headerContent}>
-                <div className={styles.headerLogo}>
-                    <img src={logo} width={50} height={50} alt="logo" />
-                    <h1>Reddit-Clone</h1>
+            <div className={styles.headerContainer}>
+                <div className={styles.headerContent}>
+                    <div className={styles.headerLogo}>
+                        <img src={logo} width={50} height={50} alt="logo" />
+                        <h1>Reddit-Clone</h1>
+                    </div>
+                    <div className={styles.headerSearch}>
+                        <form action="">
+                            <input
+                                type="search"
+                                className={styles.headerSearchInput}
+                                name=""
+                                id=""
+                            />
+                            <button type="submit">Search</button>
+                        </form>
+                    </div>
                 </div>
-                <div className={styles.headerSearch}>
-                    <form action="">
-                        <input
-                            type="search"
-                            className={styles.headerSearchInput}
-                            name=""
-                            id=""
-                        />
-                        <button type="submit">Search</button>
-                    </form>
-                </div>
-          
             </div>
         </div>
     );
